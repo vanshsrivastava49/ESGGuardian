7a07d7ed8ec52073637c20c27390e1f55fe41b38
vanshsrivastava49 <srivastavavansh11@gmail.com>
2025-04-07 17:21:16 +0530

    css changes

--- frontend/src/blockchain/uploadToBlockchain.js
+++ frontend/src/blockchain/uploadToBlockchain.js
@@ -1,12 +1,9 @@
 import { ethers } from "ethers";
-
 const contractABI = [
   "function uploadAgreement(string _fileHash, string _companyName, string _esgStatus) public",
   "function getAgreement(uint256 _id) public view returns (string, string, string, bool, address, uint256)"
 ];
-
 const contractAddress = "0xcD6a42782d230D7c13A74ddec5dD140e55499Df9";
-
 export const uploadAgreementToBlockchain = async (fileHash, companyName, esgStatus) => {
   try {
     if (!window.ethereum) {
@@ -15,15 +12,11 @@ export const uploadAgreementToBlockchain = async (fileHash, companyName, esgStat
     await window.ethereum.request({ method: "eth_requestAccounts" });
     const provider = new ethers.BrowserProvider(window.ethereum);
     const signer = await provider.getSigner();
-
     const contract = new ethers.Contract(contractAddress, contractABI, signer);
-
     const tx = await contract.uploadAgreement(fileHash, companyName, esgStatus);
     console.log("📤 Transaction sent: ", tx.hash);
-
     const receipt = await tx.wait();
     console.log("✅ Confirmed in block:", receipt.blockNumber);
-
     return { success: true, txHash: tx.hash };
   } catch (err) {
     console.error("❌ Blockchain Upload Error:", err);
